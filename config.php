9944ea46a8e84e5293727329777b590c54cb0de4
Piyush Yadav <bhavya312007@gmail.com>
2024-08-19 12:12:42 +0530

    1

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+
+  define( 'DB_HOST', 'localhost:3307' );          // Set database host
+  define( 'DB_USER', 'root' );             // Set database user
+  define( 'DB_PASS', '' );             // Set database password
+  define( 'DB_NAME', 'ReportDesk' );        // Set database name
+?>
